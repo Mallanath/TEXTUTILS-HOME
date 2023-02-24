9e9b56b97008df90d56e38f40fabd9f9ac197237
Mallanath <93929421+Mallanath@users.noreply.github.com>
2023-02-24 09:53:35 +0530

    Add files via upload

---
+++ module1.mjs
@@ -0,0 +1,2 @@
+import dzu from './module2.mjs'
+console.log(dzu);
---
+++ module2.mjs
@@ -0,0 +1,9 @@
+const a = "Mallanath";
+const b = "Harry";
+const c = "Sandhya";
+const d = "Priyanka";
+
+export default d; 
+export {a};
+export {b};
+export {c};
